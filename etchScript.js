38c157fc85c554e48adfc1ac40f5b15235b6cf49
Gamolly <gamollygamolly@gmail.com>
2022-11-06 16:35:01 -0500

    Allow multiple hovers to darken the cells.
    This was done to be closer to the act of darkening lines by going
    over them multiple times while sketching.

--- etchScript.js
+++ etchScript.js
@@ -1,12 +1,29 @@
 const divs = [];
 const divContainer = document.createElement('div');
-divContainer.classList.add('container')
+divContainer.classList.add('container');
 
-for (let i = 0; i < 16; i++){
+let initialOpacity = 0;
+
+for (let i = 0; i < 16; i++) {
     divs[i] = document.createElement('div');
     divs[i].classList.add(`div${i}`);
     divs[i].classList.add(`grid`);
+    divs[i].style.opacity = `${initialOpacity}`
+    divs[i].style.backgroundColor = 'black';
     divContainer.appendChild(divs[i]);
 }
 
 document.body.appendChild(divContainer);
+
+
+
+function darkenCell(e) {
+    if (e.target.style.opacity == '1') return;
+    else {
+        e.target.style.opacity = (parseFloat(e.target.style.opacity) + 0.1).toString();
+    }
+}
+
+divs.forEach((div) => {
+    div.addEventListener('mouseover', darkenCell);
+});
