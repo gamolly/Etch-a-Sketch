92d9f95fd37588b6b989f8491f5546450434797d
Gamolly <gamollygamolly@gmail.com>
2022-11-07 08:35:54 -0500

    Add a button allowing user to set grid resolution.

--- etchScript.js
+++ etchScript.js
@@ -1,10 +1,38 @@
 const divs = [];
+const gridBtn = document.createElement('button');
+gridBtn.classList.add('gridBtn');
+gridBtn.textContent = 'Grid Size';
+document.body.appendChild(gridBtn);
+
+let gridResolution;
+let temp;
+
+function askResolution(){
+    console.log("inside askResolution()");
+    temp = 1;
+    while(temp < 2 ||temp > 100 || !(Number.isInteger(temp)) ){
+        temp = parseInt(prompt('Please input desired grid resolution (min: 2, max: 100): '));
+        console.log(temp);
+    }
+    if (temp < 2 ||temp > 100 || typeof temp != 'number') {
+        console.log('inside if statement');
+        askResolution();
+    }
+    else gridResolution = temp;
+}
+
+gridBtn.addEventListener('click', askResolution);
+
 const divContainer = document.createElement('div');
 divContainer.classList.add('container');
 
 let initialOpacity = 0;
 
-for (let i = 0; i < 16; i++) {
+askResolution();
+
+divContainer.style.cssText = `grid-template-columns: repeat(${gridResolution},1fr)`;
+
+for (let i = 0; i < gridResolution**2; i++) {
     divs[i] = document.createElement('div');
     divs[i].classList.add(`div${i}`);
     divs[i].classList.add(`grid`);
