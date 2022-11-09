3670e27526c5f35f14633f93b202184eb57ce694
Gamolly <gamollygamolly@gmail.com>
2022-11-09 08:19:55 -0500

    Adjust button to reset size without page refresh

--- etchScript.js
+++ etchScript.js
@@ -2,48 +2,28 @@ const divs = [];
 const gridBtn = document.createElement('button');
 gridBtn.classList.add('gridBtn');
 gridBtn.textContent = 'Grid Size';
+gridBtn.setAttribute("title", "This will reset the drawing!");
 document.body.appendChild(gridBtn);
+gridBtn.addEventListener('click', askResolution);
 
 let gridResolution;
 let temp;
+let bodyNodes;
 
-function askResolution(){
+function askResolution() {
     console.log("inside askResolution()");
     temp = 1;
-    while(temp < 2 ||temp > 100 || !(Number.isInteger(temp)) ){
-        temp = parseInt(prompt('Please input desired grid resolution (min: 2, max: 100): '));
+    while (temp < 2 || temp > 100 || !(Number.isInteger(temp))) {
+        temp = parseInt(prompt('Please input desired grid resolution (min: 2, max: 100): ', '15'));
         console.log(temp);
     }
-    if (temp < 2 ||temp > 100 || typeof temp != 'number') {
+    if (temp < 2 || temp > 100 || typeof temp != 'number') {
         console.log('inside if statement');
         askResolution();
     }
     else gridResolution = temp;
+    createGrid();
 }
-
-gridBtn.addEventListener('click', askResolution);
-
-const divContainer = document.createElement('div');
-divContainer.classList.add('container');
-
-let initialOpacity = 0;
-
-askResolution();
-
-divContainer.style.cssText = `grid-template-columns: repeat(${gridResolution},1fr)`;
-
-for (let i = 0; i < gridResolution**2; i++) {
-    divs[i] = document.createElement('div');
-    divs[i].classList.add(`div${i}`);
-    divs[i].classList.add(`grid`);
-    divs[i].style.opacity = `${initialOpacity}`
-    divs[i].style.backgroundColor = 'black';
-    divContainer.appendChild(divs[i]);
-}
-
-document.body.appendChild(divContainer);
-
-
 
 function darkenCell(e) {
     if (e.target.style.opacity == '1') return;
@@ -52,6 +32,37 @@ function darkenCell(e) {
     }
 }
 
-divs.forEach((div) => {
-    div.addEventListener('mouseover', darkenCell);
-});
+
+
+let divContainer = document.createElement('div');
+divContainer.classList.add('container');
+
+let initialOpacity = 0;
+
+askResolution();
+
+divContainer.style.cssText = `grid-template-columns: repeat(${gridResolution},1fr)`;
+
+function createGrid() {
+    bodyNodes = Array.from(document.body.childNodes);
+    if (bodyNodes.includes(divContainer)){
+        console.log('bodynodes includes divcontainer');
+        document.body.removeChild(divContainer)
+    }
+    
+    divContainer = document.createElement('div');
+    divContainer.classList.add('container');
+    divContainer.style.cssText = `grid-template-columns: repeat(${gridResolution},1fr)`;
+
+    for (let i = 0; i < gridResolution ** 2; i++) {
+        divs[i] = document.createElement('div');
+        divs[i].classList.add(`div${i}`);
+        divs[i].classList.add(`grid`);
+        divs[i].style.opacity = `${initialOpacity}`
+        divs[i].style.backgroundColor = 'black';
+        divContainer.appendChild(divs[i]);
+        divs[i].addEventListener('mouseover', darkenCell);
+    }
+    document.body.appendChild(divContainer);
+}
+
